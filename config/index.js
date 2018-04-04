a4a0a95921bdd4eeebee263f7ba645705933c099
Fedor Baart <fedor.baart@deltares.nl>
2018-04-04 09:16:21 +0200

    allow to build without publicPath, for testing

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/mozambique-viewer/',
+    assetsPublicPath: '/' + (process.env.PUBLIC_PATH ? process.env.PUBLIC_PATH + '/' : ''),
 
     /**
      * Source Maps
